25c2caa657962d8eeb4a1eb2efb1a8ded441143d
Sajitha Geevinda Kodithuwakku <115799454+sajitha00@users.noreply.github.com>
2024-03-09 13:57:06 +0530

    testing read

--- lib/Services/firestore.dart
+++ lib/Services/firestore.dart
@@ -4,14 +4,17 @@ class FirestoreService{
   //get collection
   final CollectionReference notes = FirebaseFirestore.instance.collection("notes");
   //Create
-  Future<void>addNote(String note){
+  Future<void>addNote(String note) {
     return notes.add({
-      'note':note,
-      'timestamp':Timestamp.now(),
-
-
+      'note': note,
+      'timestamp': Timestamp.now(),
     });
+  }
+    //read
+    Stream<QuerySnapshot>getNote(){
+      final notestream = notes.orderBy('timestamp',descending: true).snapshots();
+      return notestream;
+    }
   }
 
 
-}
--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'Services/firestore.dart';
 import 'package:flutter/material.dart';
  class HomeScreen extends StatefulWidget {
@@ -22,7 +24,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ElevatedButton(onPressed: (){
             //add a new note
             firestoreService.addNote(textcontroller.text);
-
             //clear the text
             textcontroller.clear();
             //close the box
@@ -43,6 +44,32 @@ class _HomeScreenState extends State<HomeScreen> {
        floatingActionButton: FloatingActionButton(
          onPressed: openBox,
          child: Icon(Icons.add),
+       ),
+       body: StreamBuilder<QuerySnapshot>(
+         stream: firestoreService.getNote(),
+         builder: (context,snapshot){
+           if(snapshot.hasData){
+             List noteList = snapshot.data!.docs;
+             return ListView.builder(
+               itemCount: noteList.length,
+                 itemBuilder: (context,index){
+                   //get each individual doc
+                   DocumentSnapshot document = noteList[index];
+                   String docID = document.id;
+                   //get note from each doc
+                   Map<String,dynamic> data = document.data() as Map<String,dynamic>;
+                   String noteText = data['note'];
+                   //display as list title
+                   return ListTile(
+                     title: Text(noteText),
+                   );
+                 });
+           }
+           else{
+             return const Text("no notes");
+
+           }
+         },
        ),
      );
    }
